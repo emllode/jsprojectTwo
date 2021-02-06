6a3a0601107fa8c5bf1852af3b506d5dc40af431
Christopher Bergman <cb.christopherbergman@gmail.com>
2021-02-06 15:16:01 +0100

    Fixed some icons + some JScode to change to fahren + not has as many decimals.

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let button = document.querySelector('.submit');
-let inputValue = document.querySelector('.search');
-let name = document.querySelector('.name');
-let temp = document.querySelector('.temp');
-let desc = document.querySelector('.desc');
+const button = document.querySelector('.submit');
+const inputValue = document.querySelector('.search');
+const nameOfCity = document.querySelector('.name');
+const temp = document.querySelector('.temp');
+const desc = document.querySelector('.desc');
 
 
 button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=9c8183e6cf109ae7637aab10a479748e')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data =>  {
+        let nameValue = data['name'];
+        let tempValue = Math.round(data['main']['temp']);
+        let descValue = data['weather'][0]['description'];
 
-.catch(err => alert("Wrong city name"))
+        nameOfCity.innerHTML = nameValue;
+        temp.innerHTML = tempValue - 273;
+        desc.innerHTML = descValue;
+    })
+
+
+    
+
+.catch(err => alert("That city doesn't exist."))
 })
